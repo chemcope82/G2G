28e922c7fab494c8e7406b40f6b2737e9395a0d1
llama lord <chasejrabon@gmail.com>
2018-09-12 21:08:22 -0500

    updating champions page

--- javascript/champions.js
+++ javascript/champions.js
@@ -1,54 +1,49 @@
 console.log("connected champions.js")
-var championData = [];
-
-const API = {
+var API = {
     "key": "ede1611243a89b545af7e3cb141474ca",
     "base": "http://ddragon.leagueoflegends.com/cdn/8.17.1",
     "data": "/data/en_US",
     "images": "/img/champion"
-};
-var champions = {};
+  };
+  
+  var champions = {};
+  
+  function initUI() {
+    var containerDiv = document.createElement("div");
+    containerDiv.className = "container";
+    
+    for (var i in champions) {
+      var champion = champions[i];
+  
+      var championDiv = document.createElement("div");
+      championDiv.className = "champDiv";
+  
+      var championLink = document.createElement("a");
+      championLink.setAttribute("href", `http://api.champion.gg/v2/champions/${champion.key}?&limit=200&champData=kda,damage,averageGames,totalHeal,killingSpree,minions,gold,positions,normalized,groupedWins,trinkets,runes,firstitems,summoners,skills,finalitems,masteries,maxMins,matchups&api_key=${API.key}`);
+      championLink.setAttribute("target", "_blank");
+      championDiv.appendChild(championLink);
+      
+      var image = document.createElement("img");
+      image.setAttribute("src", `${API.base}${API.images}/${champion.image.full}`);
+      championLink.appendChild(image);
 
-function makeImages() {
-    for (i in champions) {
-        console.log(`${API.base}${API.images}/${champions[i].image.full}`);
-        championData.push(`${API.base}${API.images}/${champions[i].image.full}`);
-        $(".champions").append("<img src='" + `${API.base}${API.images}/${champions[i].image.full}` + "' >")
+      var name = document.createElement("p");
+      name.textContent = champion.name;
+      championLink.appendChild(name);
+      
+      containerDiv.appendChild(championDiv);
     }
-}
-
-function getChampionID(ddID) {
-    for (i in champions) {
-        if (champions[i].id == ddID)
-            return champions[i].key
-    }
-}
-
-function getChampionStats(championID) {
-
-    // var proxyUrl = "https://cors-ut-bootcamp.herokuapp.com/";
-    // var targetUrl = `http://api.champion.gg/v2/champions/${championID}?&limit=200&champData=kda,damage,averageGames,totalHeal,killingSpree,minions,gold,positions,normalized,groupedWins,trinkets,runes,firstitems,summoners,skills,finalitems,masteries,maxMins,matchups&api_key=${API.key}`;
-
-    // $.get(proxyUrl + targetUrl, function (data) {
-    //     console.log(data);
-    // });
-
-
-
+    document.body.appendChild(containerDiv);
+  }
+  
+  (function() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", `http://api.champion.gg/v2/champions/${championID}?&limit=200&champData=kda,damage,averageGames,totalHeal,killingSpree,minions,gold,positions,normalized,groupedWins,trinkets,runes,firstitems,summoners,skills,finalitems,masteries,maxMins,matchups&api_key=${API.key}`);
-    xhr.onload = function () {
-        console.log(JSON.parse(xhr.response));
+    xhr.open("GET", `${API.base}${API.data}/champion.json`);
+    xhr.onload = function() {
+      champions = JSON.parse(xhr.response).data;
+      initUI();
     }
     xhr.send();
-}
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", `${API.base}${API.data}/champion.json`);
-xhr.onload = function () {
-    champions = JSON.parse(xhr.response).data;
-    makeImages();
-}
-xhr.send();
+  })();
 
 
